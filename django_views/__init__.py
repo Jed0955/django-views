42bba93380d3d4348c4abceeb8f759ed0455d1a1
younger shen <younger.x.shen@gmail.com>
2017-05-31 15:09:30 +0800

    make this python package becomes a django app

--- django_views/__init__.py
+++ django_views/__init__.py
@@ -3,6 +3,6 @@
 # TIME    : 17-5-28 下午4:04
 # AUTHOR  : youngershen <younger.x.shen@gmail.com>
 
-from .views import Generic
+# from .views import Generic
 
-__all__ = ['Generic']
+# __all__ = ['Generic']
---
+++ django_views/models.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# PROJECT : haval
+# TIME    : 17-5-31 下午3:05
+# AUTHOR  : youngershen <younger.x.shen@gmail.com>
